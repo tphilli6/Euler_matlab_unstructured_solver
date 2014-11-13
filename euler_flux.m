3507464e0ba61f01953004a7eef07ca5e46946a3
Tyrone <tphilli6@gmail.com>
2014-11-12 16:10:33 -0800

    to separate the euler flux and the mms euler flux routines

---
+++ euler_flux.m
@@ -0,0 +1,22 @@
+function flux = euler_flux(rho, u, v, p, normal)
+% Computes the Euler analytic flux for a given function
+% Inputs:
+%       rho : Density
+%       u     X-velocity
+%       v     Y-Velocity
+%       p     Pressure
+%    normal : face normal [2x1]
+%
+% Outputs:
+%      flux : analytic flux
+% HARDWIRE: gamma = 1.4
+gamma = 1.4;
+
+
+v_norm = u*normal(1) + v*normal(2);
+ht = gamma/(gamma-1)*p/rho + 0.5*(u^2 + v^2);
+
+flux(1) = rho*v_norm;
+flux(2) = rho*u*v_norm + normal(1)*p;
+flux(3) = rho*v*v_norm + normal(2)*p;
+flux(4) = rho*ht*v_norm;
