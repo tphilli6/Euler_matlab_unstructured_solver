c3639245416def0ba3db0a3f0e72ee0f675a4272
Tyrone <tphilli6@gmail.com>
2015-05-18 21:59:57 -0700

    in the process of getting the smooth grid te working

--- te_smooth_grid.m
+++ te_smooth_grid.m
@@ -5,25 +5,27 @@ function te_smooth_grid(cell, face, vertex, ...
                         te_order,...
                         flux_integral_order,...
                         analytic_soln,...
-                        flux)
+                        flux,...
+                        vertex_centered,...
+                        cell_grid,...
+                        face_grid,...
+                        vertex_grid,...
+                        cell_cv_to_tri)
  
+                    
 itermax = 1;
                     
 eq = {'mass','xmtm', 'ymtm', 'nrgy'};
+var = {'rho','u','v','p'};
 
+base_order = kexact_order;
+base_type  = kexact_type;
+base_fit_type = fit_type;
+soln_old = cell.soln;
+cell_old = cell;
+face_old = face;
+vertex_old = vertex;
 
-% base_order = kexact_order;
-% base_type  = kexact_type;
-% base_fit_type = fit_type;
-% soln_old = cell.soln;
-% cell_old = cell;
-% face_old = face;
-% vertex_old = vertex;
-
-% xmat = reshape(cell_old.xc(:,1),[8,8]);
-% ymat = reshape(cell_old.xc(:,2),[8,8]);
-% s = reshape(cell_old.soln(:,4), [8,8]);
-% v = reshape(cell_old.volume, [8,8]);
 
   % Step 1: compute a lsq reconstruction over a large stencil
   % - setup_reconstruction sets up the polynomial for the reconstruction
@@ -38,72 +40,83 @@ eq = {'mass','xmtm', 'ymtm', 'nrgy'};
   % reconstruct the higher order solution
   % Sets up higher_order_recon(n).coef  % coefficients of higher order reconstruction
   cell.lhs_set = 0; % Reset stencil
-  [higher_order_recon, ~] = reconstruct_solution(cell, fit_type);
-%   for i = 1:length(higher_order_recon)
-% %       higher_order_recon(i).coef(:,1) = [1, 0, -0.15, 0, 0, 0.1]';
-% %       higher_order_recon(i).coef(:,2) = [800, 0, 50, 0, 0, 30]';
-% %       higher_order_recon(i).coef(:,3) = [800, 0, -75, 0, 0, 40]';
-% %       higher_order_recon(i).coef(:,4) = [100000, 0, 20000, 0, 0, 50000]';
-%       higher_order_recon(i).coef(:,1) = [1, -0.15, 0, 0.1, 0, 0]';
-%       higher_order_recon(i).coef(:,2) = [800, 50, 0, 30, 0, 0]';
-%       higher_order_recon(i).coef(:,3) = [800, -75, 0, 40, 0, 0]';
-%       higher_order_recon(i).coef(:,4) = [100000, 20000, 0, 50000, 0, 0]';
-%   end
-  cell.reconstruction = higher_order_recon;
-   
-   
-   
+  [higher_order_recon, ~] = reconstruct_solution(cell, fit_type, 1);
 
-  face_out = compute_left_and_right_state(vertex, cell, face,...
-                                          analytic_soln);
-  face = face_out;
-
-  % This is just a regular higher order estimate
-  te_standard = compute_residual( cell, face, flux );
-
-  write_vtk_solution( vertex_old, cell_old, te_standard, 'te_estimate_k4', 'grid.vtk', 'a', eq )
+% ***********************************************
+% ***********************************************
+% Compute the jacobian for the original triangle mesh
+for i = 1:cell_grid.ncells
+    x = vertex(cell_grid.nodes(i,2:4),1);
+    y = vertex(cell_grid.nodes(i,2:4),2);
+    J(:,:,i) = compute_triangle_jacobian(x,y);
+end
 
 
 
-  % Step 1: compute a lsq reconstruction over a large stencil
-  % - setup_reconstruction sets up the polynomial for the reconstruction
-  kexact_order = te_order;
-  kexact_type  = base_type;
-  fit_type = 'lsq-true';
- 
-  setup_reconstruction;
-  higher_order_recon_param = cell.reconstruction_param;
-  build_kexact_stencil;
+% cell_grid_to_cv = zeros(cell.ncells,1);
+% for i = 1:cell.ncells
+%     for j = 1:cell.nodes(i,1)
+%       test_mat = repmat( vertex(cell.nodes(i,j+1),1:2), [cell_grid.ncells,1]);
+%       I = find(  test_mat == cell_grid.xc(:,1:2) );
+%       if length(I)>0
+%           lcgc = length( find(cell_grid_to_cv(i,:)~=0) ) + 1;
+%           cell_grid_to_cv(i,lcgc) = I(1);
+%       end
+%       
+%     end
+% end
+% 
+% I = find(cell_grid_to_cv(1,:)~=0);
+% Jave = mean( J(:,:,cell_grid_to_cv(1,I)),3 );
+% Jave = J(:,:,1);
 
+[ti, xi, icell] = generate_equilateral_mesh(te_order);
+% volume(x)/det(J) = volume(xi)
+% scale jacobian so that the areas are the same even if the shape is not.
+dxideta = 0.866025403784439; %6.4952; %det( [ 0.5, 0; 0.25, 0.5*tand(60)] ); % Volume of equilateral triangle from generate_equilateral_mesh()
+icell = floor(size(xi,1)/2);
 
-  % ******* Important output ********* higher_order_recon
-  % reconstruct the higher order solution
-  % Sets up higher_order_recon(n).coef  % coefficients of higher order reconstruction
-  cell.lhs_set = 0; % Reset stencil
-  [higher_order_recon, ~] = reconstruct_solution(cell, fit_type);
-%   for i = 1:length(higher_order_recon)
-%       higher_order_recon(i).coef(:,1) = [1, 0, -0.15, 0, 0, 0.1]';
-%       higher_order_recon(i).coef(:,2) = [800, 0, 50, 0, 0, 30]';
-%       higher_order_recon(i).coef(:,3) = [800, 0, -75, 0, 0, 40]';
-%       higher_order_recon(i).coef(:,4) = [100000, 0, 20000, 0, 0, 50000]';
-%       higher_order_recon(i).coef(:,1) = [1, -0.15, 0, 0.1, 0, 0]';
-%       higher_order_recon(i).coef(:,2) = [800, 50, 0, 30, 0, 0]';
-%       higher_order_recon(i).coef(:,3) = [800, -75, 0, 40, 0, 0]';
-%       higher_order_recon(i).coef(:,4) = [100000, 20000, 0, 50000, 0, 0]';
-      
-%   end
-   cell.reconstruction = higher_order_recon;
-  
-%      % Copy the old cell setup for the same reconstruction stencil that was used for the solve
-%   cell = cell_old;
-  
-% for iter = 1:itermax
-  % Copy the old cell setup for the same reconstruction stencil that was used for the solve
-%   cell = cell_old;
-  
+vertex = [xi,zeros(size(xi(:,1)))];
+
+[face_tri, cell_tri] =  compute_face_data_from_triangulation(ti,vertex);
+[cell, face, vertex, cell_all, all_to_cv] = compute_vertex_centered_nonsense(cell_tri, face_tri, vertex);
+cell = find_neighbor_cells(cell, face);
+cell.soln = zeros( cell.ncells, size(cell_old.soln,2) );
+
+% cell_te, face_te, and vertex_te store information that's required for
+% higher order reconstruction
+kexact_order = te_order;
+kexact_type = base_type;
+setup_reconstruction;
+build_kexact_stencil;
+cell_te = cell;
+
+% cell_smooth stores the solver order information for the smooth grid
+kexact_order = base_order;
+kexact_type = base_type;
+setup_reconstruction;
+build_kexact_stencil;
+
+stencil=cell.stencil(icell).cells;
+for i = cell.stencil(icell).cells
+    stencil = [stencil, cell.stencil(i).cells];
+end
+stencil = unique(stencil);
+
+I = find(stencil~=icell);
+stencil = [icell, stencil(I)];
+
+% WAIT. WAIT. WAIT. I'm not quite sure if my process is correct.
+
+cell_smooth = cell;
+face_smooth = face;
 for n = 1:cell.ncells
+    cell = cell_smooth;
+    face = face_smooth;
+
+
 fprintf('\nTE estimate for cell %4.0f\n',n);
-  % Euler hardcode
+  % Euler hardcode for source term
   func.rho = @(x) pbase(x, higher_order_recon(n).coef(:,1), higher_order_recon_param.px, higher_order_recon_param.py);
   func.u = @(x)   pbase(x, higher_order_recon(n).coef(:,2), higher_order_recon_param.px, higher_order_recon_param.py);
   func.v = @(x)   pbase(x, higher_order_recon(n).coef(:,3), higher_order_recon_param.px, higher_order_recon_param.py);
@@ -111,17 +124,55 @@ fprintf('\nTE estimate for cell %4.0f\n',n);
   exact_flux = @(x, normal) euler_mms_flux(x, func, normal);
   neq = 4;
 
-%   tic
-%   higher_order_recon(n).coef;
-% 
-  cell = cell_old;
-  vertex = vertex_old;
-  face = face_old;
-  
-  kexact_order = base_order;
-  kexact_type  =  base_type;
-  fit_type = base_fit_type;
+  % Compute scaling
+  scale = sqrt( cell_old.volume(n)/dxideta );
+%   scale = 1;
+  vertex_scaled(:,1) = (vertex(:,1)-cell_smooth.xc(icell,1) )*scale + cell_old.xc(n,1);
+  vertex_scaled(:,2) = (vertex(:,2)-cell_smooth.xc(icell,2) )*scale + cell_old.xc(n,2);
+  vertex_scaled(:,3) = 0;
 
+  for i = 1:cell.ncells
+      cell.volume(i) = cell.volume(i)*scale^2;
+      I = find( cell.nodes(i,:)~=0 );
+      cell.xc(i,:) = mean( vertex_scaled( cell.nodes(i,I), 1:2), 1);
+  end
+  
+  for i = 1:length(face)
+      face(i).area = face(i).area*scale;
+  end
+%       nnodes = cell.nodes(i,1);
+%       inodes = cell.nodes(i,2:nnodes+1);
+%       cell.xc(i,1) = mean( vertex_scaled(inodes,1) );
+%       cell.xc(i,2) = mean( vertex_scaled(inodes,2) );
+%       cell.volume(i) = 0;
+%       
+%       I = find(cell.faces(n,:)~=0);
+%       nf = length(I);
+%       f = cell.faces(n,I);
+%       for j = f
+%           fn = face_smooth(f).nodes;
+%           ax = vertex_scaled(fn(1),1);
+%           ay = vertex_scaled(fn(1),2);
+%           bx = vertex_scaled(fn(2),1);
+%           by = vertex_scaled(fn(2),2);
+%           cx = cell.xc(i,1);
+%           cy = cell.xc(i,2);
+%           
+% %           a = compute_area(ax,ay,bx,by,cx,cy);
+%           a = abs( triangle_area([ax,ay,0], [bx,by,0], [cx,cy,0]) );
+%           cell.volume(i) = cell.volume(i) + a;
+%       end
+%       
+%   end
+  
+  [face, ~] =  compute_face_data_from_triangulation(cell.nodes(:,2:end),vertex_scaled);
+%   for i = 1:length(face_smooth)
+%          fn = face_smooth(i).nodes;
+%          ds_vec = vertex_scaled(fn(2),:) - vertex_scaled(fn(1),:);
+%          face_smooth(i).area = sqrt( sum(ds_vec.^2) );
+%          face_smooth(i).normal(1:2) = [ ds_vec(2), -ds_vec(1) ]/face(i).area;
+%   end
+  
 %   cell.mms_source = analytic_flux(vertex, cell, face, exact_flux, neq,...
 %                                   flux_integral_order, n);
 
@@ -135,101 +186,119 @@ fprintf('\nTE estimate for cell %4.0f\n',n);
   %      subcell.reconstruction_param.py    % y coefficients
   %      subcell.reconstuction.Ai           % integral over structured cell (smooth) geometry
   %      subcell.reconstruction.Axc         % reference integral location at cell center
-  [cell, face, vertex, icell, cell_phy] = construct_te_smooth_stencil(...
-                                 cell_old, face_old, vertex_old,...
-                                 te_order, ...
-                                 kexact_type, ...
-                                 fit_type, ...
-                                 flux_integral_order, base_order, n );
+%   [cell, face, vertex, icell, cell_phy] = construct_te_smooth_stencil(...
+%                                  cell_old, face_old, vertex_old,...
+%                                  te_order, ...
+%                                  kexact_type, ...
+%                                  fit_type, ...
+%                                  flux_integral_order, base_order, n );
 %   imax_local = 2*(te_order + 1)+1;
 %   row = (imax_local-1)/2;
 %   cntr = (imax_local)*row + row + 1;
 %   ivec = (icell-1)*(imax_local) + icell;
                              
-                             
-  cell.mms_source = analytic_flux(vertex, cell, face, exact_flux, neq,...
+  % Need to shift vertex_scaled => vertex_scaled - xc_{cur_cell}   
+  cell.mms_source = analytic_flux(vertex_scaled, cell, face, exact_flux, neq,...
                                   flux_integral_order, icell);
 
 % mms_source_test = analytic_flux(vertex_old, cell_old, face_old, exact_flux, neq,...
 %                                   flux_integral_order,n);
+
+
+% SOMETHING IS WRONG HERE.
   
-  % Step 5: Evaluate the higher order reconstruction over the smooth cell stencils.
-  for i = 1:cell.ncells
-    for nneq = 1:size(higher_order_recon(n).coef,2)
-      cell.soln(i,nneq) = cell_phy.reconstruction(i).Ai*higher_order_recon(n).coef(:,nneq);
-    end
-  end
+pte(1,:) = cell_te.reconstruction_param.px;
+pte(2,:) = cell_te.reconstruction_param.py;
+
+% [recon_soln_eval, ~] = reconstruct_solution(cell, fit_type, 1);
+[moment] = compute_reconstruction_moments(vertex_scaled, cell, face, pte );
+[~, ~, Aeval] = compute_reconstruction_lhs(stencil, icell, moment, pte, cell.xc, 1);
+
+% for i = 1:size(A,1)
+%     A(i,:) = A(i,:)./wij(i);
+% end
 
 
-  kexact_order = base_order;
-%   kexact_type  = 'kexact_extended';%base_type;
-%   fit_type = 'kexact';%base_fit_type;
-  kexact_type  = base_type;
-  fit_type = base_fit_type;
-  
-  setup_reconstruction;
-%   build_kexact_stencil;
-
-if cell_old.cell_type(n) == 0
-  sten = base_order+2;
-  sten_width = floor(sten/2);
-  cnt2=1;
-  for jj = row:row+2
-      for ii = row:row+2
-% %   for jj = 1:imax_local
-% %       for ii = 1:imax_local
-          iivec = (jj-1)*(imax_local) + ii;
-%           
-%           ilow = ii - sten_width;
-%           ihigh = ilow + sten;
-%           if (ilow<1)
-%               ilow = 1;
-%               ihigh = ilow + sten;
-%           elseif (ihigh>imax_local)
-%               ihigh = imax_local;
-%               ilow = ihigh - sten;
-%           end
-%           
-%           jlow = jj - sten_width;
-%           jhigh = jlow + sten;
-%           if (jlow<1)
-%               jlow = 1;
-%               jhigh = jlow + sten;
-%           elseif (jhigh>imax_local)
-%               jhigh = imax_local;
-%               jlow = jhigh - sten;
-%           end
-%           
-%           cnt = 1;
-%           for jjj = jlow : jhigh
-%               for iii = ilow : ihigh              
-%                   iiivec = (jjj-1)*(imax_local) + iii;
-%                   sub_sten(cnt) = iiivec;
-%                   cnt = cnt + 1;
-%               end
-%           end  
-%             cell.stencil(iivec).cells = sub_sten;
-% 
-            recon_sten(cnt2)=iivec;
-            cnt2 = cnt2 + 1;
-      end
-  end
-  
-elseif cell_old.cell_type(n) == 1
-
-    cell_faces = cell.faces(icell,:);
-    recon_sten(1) = icell;
-    for jj = 1:length(cell_faces)
-        recon_sten(jj+1) = setdiff([face(cell_faces(jj)).cell_neg, face(cell_faces(jj)).cell_plus],icell);
-    end
+% Step 5: Evaluate the higher order reconstruction over the smooth cell stencils.
+for i = 1:size(higher_order_recon(1).coef,2)
+    cell_eval = Aeval*higher_order_recon(n).coef(:,i);
+    cell.soln(stencil,i) = cell_eval;  
 end
 
-  cell.lhs_set = 0; % Reset stencil
-  [reconstruction, ~] = reconstruct_solution(cell, fit_type, recon_sten);
-  cell.reconstruction = reconstruction;
+
+  [smooth_recon, ~] = reconstruct_solution(cell, fit_type, 1, cell.stencil(icell).cells);
 
 
+%   kexact_order = base_order;
+%   kexact_type  = 'kexact_extended';%base_type;
+%   fit_type = 'kexact';%base_fit_type;
+%   kexact_type  = base_type;
+%   fit_type = base_fit_type;
+  
+%   setup_reconstruction;
+% %   build_kexact_stencil;
+% 
+% if cell_old.cell_type(n) == 0
+%   sten = base_order+2;
+%   sten_width = floor(sten/2);
+%   cnt2=1;
+%   for jj = row:row+2
+%       for ii = row:row+2
+% % %   for jj = 1:imax_local
+% % %       for ii = 1:imax_local
+%           iivec = (jj-1)*(imax_local) + ii;
+% %           
+% %           ilow = ii - sten_width;
+% %           ihigh = ilow + sten;
+% %           if (ilow<1)
+% %               ilow = 1;
+% %               ihigh = ilow + sten;
+% %           elseif (ihigh>imax_local)
+% %               ihigh = imax_local;
+% %               ilow = ihigh - sten;
+% %           end
+% %           
+% %           jlow = jj - sten_width;
+% %           jhigh = jlow + sten;
+% %           if (jlow<1)
+% %               jlow = 1;
+% %               jhigh = jlow + sten;
+% %           elseif (jhigh>imax_local)
+% %               jhigh = imax_local;
+% %               jlow = jhigh - sten;
+% %           end
+% %           
+% %           cnt = 1;
+% %           for jjj = jlow : jhigh
+% %               for iii = ilow : ihigh              
+% %                   iiivec = (jjj-1)*(imax_local) + iii;
+% %                   sub_sten(cnt) = iiivec;
+% %                   cnt = cnt + 1;
+% %               end
+% %           end  
+% %             cell.stencil(iivec).cells = sub_sten;
+% % 
+%             recon_sten(cnt2)=iivec;
+%             cnt2 = cnt2 + 1;
+%       end
+%   end
+%   
+% elseif cell_old.cell_type(n) == 1
+% 
+%     cell_faces = cell.faces(icell,:);
+%     recon_sten(1) = icell;
+%     for jj = 1:length(cell_faces)
+%         recon_sten(jj+1) = setdiff([face(cell_faces(jj)).cell_neg, face(cell_faces(jj)).cell_plus],icell);
+%     end
+% end
+% 
+%   cell.lhs_set = 0; % Reset stencil
+%   [reconstruction, ~] = reconstruct_solution(cell, fit_type, recon_sten);
+%   cell.reconstruction = reconstruction;
+% 
+% 
 
+  cell.reconstruction = smooth_recon;
   % Step 6: Evaluate the residual over the smooth cells
   ncell = cell.faces(icell,:);
   face_out = compute_left_and_right_state(vertex, cell, face,...
@@ -243,6 +312,14 @@ end
 
 %   ivec = (icell-1)*(imax_local) + icell;
   te_smooth(n,:) = -te_temp(icell,:);
+  
+data_write = vertex_to_cell_average(cell.soln,cell_tri);
+write_vtk_solution( vertex_scaled, cell_tri, data_write, 'soln', ['soln-te-',num2str(n),'.vtk'],'w',var ) 
+
+data_write = vertex_to_cell_average(cell.mms_source,cell_tri);
+write_vtk_solution( vertex_scaled, cell_tri, data_write, 'source', ['soln-te-',num2str(n),'.vtk'],'a',eq ) 
+
+
 %   te_smooth(n,:) = cell.soln(ivec,:);
 %    te_smooth(n,:) = cell.mms_source(ivec,:);
 % R = reshape(te_temp(:,1), [imax_local,imax_local]);
